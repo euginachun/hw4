f6a426454c12a884fa66e8249687fa519371d142
euginachun <35680934+euginachun@users.noreply.github.com>
2018-02-13 00:47:38 -0800

    Create musical-instrument.js
    you can layer the sounds. in order to turn off the sounds, press and hold the corresponding key.

---
+++ musical-instrument.js
@@ -0,0 +1,114 @@
+
+var freqA = 174;
+var freqS = 196;
+var freqD = 220;
+var freqF = 246;
+
+var oscA, oscS, oscD, oscF;
+
+var playingA = false;
+var playingS = false;
+var playingD = false;
+var playingF = false;
+
+
+function setup() {
+  backgroundColor = color(255, 0, 255);
+  textAlign(CENTER);
+  
+  oscA = new p5.Oscillator();
+  oscA.setType('triangle');
+  oscA.freq(freqA);
+  oscA.amp(0);
+  oscA.start();
+  
+  oscS = new p5.Oscillator();
+  oscS.setType('triangle');
+  oscS.freq(freqS);
+  oscS.amp(0);
+  oscS.start();
+  
+  oscD = new p5.Oscillator();
+  oscD.setType('triangle');
+  oscD.freq(freqD);
+  oscD.amp(0);
+  oscD.start();
+  
+  oscF = new p5.Oscillator();
+  oscF.setType('triangle');
+  oscF.freq(freqF);
+  oscF.amp(0);
+  oscF.start();
+}
+
+function draw() {
+  if (playing) {
+    background(0, 255, 255);
+  } else {
+    background(255, 0, 255);
+  }
+  text('click here,\nthen press A/S/D/F\n keys to play', width / 2, 40);
+}
+
+function keyPressed() {
+  print("got key press for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+    playingA = true;
+  } else if (key == 'S') {
+    osc = oscS;
+    playingS = true;
+  } else if (key == 'D') {
+    osc = oscD;
+    playingD = true;
+  } else if (key == 'F') {
+    osc = oscF;
+    playingF = true;
+  }
+  if (osc) {
+    osc.amp(.5, 1);
+
+  }
+}
+function draw() {
+  background(255);
+  fill("pink");
+  if (playingA) {
+    ellipse(50, 50, 25, 25);
+  }
+  fill("lightGreen");
+  if (playingS) {
+    ellipse (50, 50, 50, 50);
+  }
+  fill("purple");
+  if (playingD) {
+    ellipse (50, 50, 75, 75);
+  }
+  
+  fill("lightBlue");
+  if (playingF) {
+    ellipse (50, 50, 100, 100);
+  }
+}
+
+function keyReleased() {
+  print("got key release for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+  } else if (key == 'S') {
+    osc = oscS;
+  } else if (key == 'D') {
+    osc = oscD;
+  } else if (key == 'F') {
+    osc = oscF;
+  }
+  if (osc) {
+    osc.amp(0, .5);
+    playingA = false;
+    playingS = false;
+    playingD = false;
+    playingF = false;
+  }
+}
